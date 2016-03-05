143ec607ec6b3b8f7193b8f5faa31ce81aa1211c
Matt Condon <mattgcondon@gmail.com>
2016-03-05 17:23:58 -0500

    add process.env.PORT and start script

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var port = server.address().port;
 
   console.log('Example app listening an :%s', port);
